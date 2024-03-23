b3d7b9ad42204483e0087b7b2107e8b30a72b420
Dim0000 <nh2fuji+github@gmail.com>
2024-03-23 11:26:11 +0900

    fix database.ts

--- utils/database.ts
+++ utils/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI ?? "")
     console.log("Success: Connected to MongoDB")
   } catch (err) {
     console.log("Failure: Unconected to MongoDB")
